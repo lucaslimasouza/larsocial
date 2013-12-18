ac2cbda4e7ed1540cce83326dd40a78a1b05cf15
Lucas Lima de Souza <lucaslima4p@gmail.com>
2013-12-17 23:57:29 -0200

    methods scaffold and validation presence of tests

--- app/models/condominium.rb
+++ app/models/condominium.rb
@@ -1,2 +1,6 @@
 class Condominium < ActiveRecord::Base
+
+	validates_presence_of :address, :number, :zip_code, :building,
+		:designation_building
+
 end
--- spec/models/condominium_spec.rb
+++ spec/models/condominium_spec.rb
@@ -5,5 +5,16 @@ describe Condominium do
 	context "methods" do
 		it { should respond_to(:address) }
 		it { should respond_to(:number) }
+		it { should respond_to(:zip_code) }
+		it { should respond_to(:building) }
+		it { should respond_to(:designation_building) }
+	end
+
+	context "validations" do
+		it { should validate_presence_of(:address) }
+		it { should validate_presence_of(:number) }
+		it { should validate_presence_of(:zip_code) }
+		it { should validate_presence_of(:building) }
+		it { should validate_presence_of(:designation_building) }
 	end
 end
