06a4d56bb4f86bf103ebcafd66aeb346afc538f4
Lucas Lima de Souza <lucaslima4p@gmail.com>
2014-01-11 16:44:07 -0200

    Dweller::RegistrationController nested attributes for Apartment

--- app/controllers/dwellers/registrations_controller.rb
+++ app/controllers/dwellers/registrations_controller.rb
@@ -1,5 +1,39 @@
 
 class Dwellers::RegistrationsController < Devise::RegistrationsController
 
+	def new
+    build_resource({})
+    self.resource.build_apartment
+    respond_with self.resource
+  end
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+
+    if resource.save
+      yield resource if block_given?
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+        expire_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      resource.apartment
+      respond_with resource
+    end
+  end
+
+  def sign_up_params
+    params.
+      require(:dweller).
+      permit(:name, :phone, :cpf,  :email, :password, :password_confirmation,
+        apartment_attributes: [:number, :designation_building])
+  end
 
 end
